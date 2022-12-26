dc56b3983abc0b93b648accf97f41e6459f64732
Alisa Knyazeva <106866969+cruelotter@users.noreply.github.com>
2022-12-27 02:23:46 +0300

    Delete build directory

--- build/lib/analysisbot/model/parsers/__init__.py
+++
@@ -1,6 +0,0 @@
-from analysisbot.model.parsers.telegram import ParserTelegram
-from analysisbot.model.parsers.vk import ParserVK
-from analysisbot.model.parsers.tinkoff import ParserTinkoff
-from analysisbot.model.parsers.parser import Parser
-
-__all__ = ['Parser', 'ParserTelegram', 'ParserVK', 'ParserTinkoff']
